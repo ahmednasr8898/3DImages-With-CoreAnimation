f90aec2823e0be0d93f03c994785e0edb0f09009
ahmednasr <ahmed.nasr.saieed@gmail.com>
2024-04-18 10:38:38 +0200

    Update ViewController.swift

--- 3DImagesWithCoreAnimation/ViewController.swift
+++ 3DImagesWithCoreAnimation/ViewController.swift
@@ -13,17 +13,30 @@ func degreeToRadians(degree: CGFloat) -> CGFloat{
 
 class ViewController: UIViewController {
     
+    // MARK: Proprites
+    
     var transformLayer = CATransformLayer()
     var currentAngle: CGFloat = 0
     var currentOffset: CGFloat = 0
-
-    override var preferredStatusBarStyle: UIStatusBarStyle{
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
     
+    // MARK: Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configurations()
+        configureTapAction()
+    }
+}
+
+
+// MARK: Configurations
+
+extension ViewController {
+    private func configurations() {
         transformLayer.frame = self.view.bounds
         view.layer.addSublayer(transformLayer)
         
@@ -32,34 +45,34 @@ class ViewController: UIViewController {
         }
         
         turnCarousal()
-        
+    }
+    
+    private func configureTapAction() {
         let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(ViewController.performPanAction(recognizer:)))
         self.view.addGestureRecognizer(panGestureRecognizer)
     }
     
-    func setImageCard(imageName: String){
+    @objc private func performPanAction(recognizer: UIPanGestureRecognizer) {
+        let xOffset = recognizer.translation(in: self.view).x
         
-        let imageCardSize = CGSize(width: 200, height: 300)
+        if recognizer.state == .began{
+            currentOffset = 0
+        }
         
-        let imageLayer = CALayer()
-        imageLayer.frame = CGRect(x: view.frame.size.width / 2 - imageCardSize.width / 2, y: view.frame.size.height / 2 - imageCardSize.height / 2, width: imageCardSize.width, height: imageCardSize.height)
-    
-        imageLayer.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        let xDifference = xOffset * 0.6 - currentOffset
         
-        guard let imageCardImage = UIImage(named: imageName)?.cgImage else {return}
+        currentOffset += xDifference
+        currentAngle += xDifference
         
-        imageLayer.contents = imageCardImage
-        imageLayer.contentsGravity = .resizeAspectFill
-        imageLayer.masksToBounds = true
-        imageLayer.isDoubleSided = true
-        imageLayer.borderColor = UIColor(white: 1, alpha: 0.5).cgColor
-        imageLayer.borderWidth = 5
-        imageLayer.cornerRadius = 10
-        
-        transformLayer.addSublayer(imageLayer)
+        turnCarousal()
     }
-    
-    func turnCarousal(){
+}
+
+
+// MARK: Private handlers
+
+extension ViewController {
+    private func turnCarousal() {
         guard let transformSublayers = transformLayer.sublayers else {return}
         
         let segmentForImageCard = CGFloat(360 / transformSublayers.count)
@@ -80,19 +93,26 @@ class ViewController: UIViewController {
         }
     }
     
-    @objc func performPanAction(recognizer: UIPanGestureRecognizer){
-        let xOffset = recognizer.translation(in: self.view).x
+    private func setImageCard(imageName: String){
         
-        if recognizer.state == .began{
-            currentOffset = 0
-        }
+        let imageCardSize = CGSize(width: 200, height: 300)
         
-        let xDifference = xOffset * 0.6 - currentOffset
+        let imageLayer = CALayer()
+        imageLayer.frame = CGRect(x: view.frame.size.width / 2 - imageCardSize.width / 2, y: view.frame.size.height / 2 - imageCardSize.height / 2, width: imageCardSize.width, height: imageCardSize.height)
+    
+        imageLayer.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         
-        currentOffset += xDifference
-        currentAngle += xDifference
+        guard let imageCardImage = UIImage(named: imageName)?.cgImage else {return}
         
-        turnCarousal()
+        imageLayer.contents = imageCardImage
+        imageLayer.contentsGravity = .resizeAspectFill
+        imageLayer.masksToBounds = true
+        imageLayer.isDoubleSided = true
+        imageLayer.borderColor = UIColor(white: 1, alpha: 0.5).cgColor
+        imageLayer.borderWidth = 5
+        imageLayer.cornerRadius = 10
+        
+        transformLayer.addSublayer(imageLayer)
     }
 }
-
+ 
